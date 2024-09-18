dbbfca87c3c529dd491a0506c0664dd4dbf85980
Seva Deriushkin <vsevolod.deriushkin@gmail.com>
2024-09-18 16:13:27 +0200

    fix Fraction export

--- classes/index.ts
+++ classes/index.ts
@@ -1,2 +1,1 @@
 export * from "./Set";
-export * as Fraction from "fraction.js";
--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
+import Fraction from "fraction.js";
+
 export * from "./lib";
 export * from "./classes";
+export { Fraction };
