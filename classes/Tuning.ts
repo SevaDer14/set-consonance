df5748eea7aa722fc7e50a5eda879da50e5eca9c
Seva Deriushkin <vsevolod.deriushkin@gmail.com>
2025-01-15 18:14:31 +0100

    fixes tuning algorithms

--- classes/Tuning.ts
+++ classes/Tuning.ts
@@ -1,13 +1,22 @@
 import Fraction from "fraction.js";
 import { Set } from "./Set";
-import { getAffinity, getConsonance, getHarmonicity } from "../lib";
+import { getConsonance } from "../lib";
+
+type SortBy = "interval" | "consonance";
 
 type HarmonicTuningOptions = {
-  precision: number;
+  precision?: number;
+  octaveSpan?: number;
 };
 
-const DEFAULT_HARMONIC_TUNING_OPTIONS: HarmonicTuningOptions = {
+const DEFAULT_HARMONIC_TUNING_OPTIONS: Required<HarmonicTuningOptions> = {
   precision: 100,
+  octaveSpan: 1,
+};
+
+const DEFAULT_CONSTRUCTOR_OPTIONS = {
+  affinityWeight: 0.5,
+  harmonicityWeight: 0.5,
 };
 
 export class Tuning {
@@ -16,11 +25,11 @@ export class Tuning {
   affinityWeight: number;
   harmonicityWeight: number;
 
-  constructor({ affinityWeight = 0.5, harmonicityWeight = 0.5 }) {
+  constructor(options = DEFAULT_CONSTRUCTOR_OPTIONS) {
     this.intervals = new Set();
     this.consonanceValues = new Map();
-    this.affinityWeight = affinityWeight;
-    this.harmonicityWeight = harmonicityWeight;
+    this.affinityWeight = options.affinityWeight;
+    this.harmonicityWeight = options.harmonicityWeight;
   }
 
   affinitive(set1: Set, set2: Set) {
@@ -52,18 +61,37 @@ export class Tuning {
     return this;
   }
 
-  harmonic(set1: Set, set2: Set, options = DEFAULT_HARMONIC_TUNING_OPTIONS) {
+  harmonic(set1: Set, set2: Set, opt?: HarmonicTuningOptions) {
+    const options = { ...DEFAULT_HARMONIC_TUNING_OPTIONS, ...(opt || {}) };
+
     this.intervals = new Set();
     const octaveTuning = new Set();
 
-    for (let i = 0; i < options.precision; i++) {
-      octaveTuning.add(new Fraction(options.precision + i, options.precision));
+    for (let denominator = 1; denominator <= options.precision; denominator++) {
+      for (
+        let numerator = denominator;
+        numerator <= denominator * 2;
+        numerator++
+      ) {
+        octaveTuning.add(new Fraction(numerator, denominator));
+      }
     }
 
-    this.intervals.union([
-      octaveTuning.toShifted(new Fraction(1, 2)),
-      octaveTuning,
-    ]);
+    const octaveTunings: Set[] = [];
+
+    for (
+      let octave = options.octaveSpan;
+      octave > -options.octaveSpan;
+      octave--
+    ) {
+      const interval =
+        octave > 0
+          ? new Fraction(2 ** octave, 1)
+          : new Fraction(1, 2 ** -octave);
+      octaveTunings.push(octaveTuning.toShifted(interval));
+    }
+
+    this.intervals = this.intervals.union(octaveTunings);
 
     this.intervals.fractions.forEach((fraction, key) => {
       this.consonanceValues.set(
@@ -82,5 +110,23 @@ export class Tuning {
 
   superset(set1: Set, set2: Set) {
     return this.affinitive(set1.harmonicSuperset(), set2.harmonicSuperset());
+  }
+
+  toFileFormat(options: { sortBy: SortBy }) {
+    let items: [Fraction, number][] = [];
+
+    this.intervals.fractions.forEach((fraction, key) => {
+      const consonance = this.consonanceValues.get(key)?.valueOf();
+
+      if (!consonance) return;
+
+      items.push([fraction, consonance]);
+    });
+
+    items.sort((a, b) =>
+      options.sortBy === "consonance" ? b[1] - a[1] : a[0].compare(b[0])
+    );
+
+    return items.map(([fraction, consonance]) => `${fraction.toFraction()}, ${fraction.valueOf()}, ${consonance}`).join("\n");
   }
 }
